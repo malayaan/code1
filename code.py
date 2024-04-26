a9f7e29128a218e8a47124cadc5d8e2bd16a6ce0
malayaan <117047767+malayaan@users.noreply.github.com>
2024-04-26 19:48:46 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -4,28 +4,31 @@ import plotly.express as px
 # et 'unique_product_types' contient vos étiquettes correspondantes.
 
 # Nous créons un DataFrame pour faciliter la manipulation avec Plotly
-df_embeddings = pd.DataFrame(embeddings_3d, columns=['x', 'y', 'z'])
+df_embeddings = pd.DataFrame(embeddings_3d, columns=['PC1', 'PC2', 'PC3'])
 df_embeddings['product_type'] = unique_product_types  # Ajout des étiquettes
 
-# Création du graphique
+# Création du graphique avec une palette de couleurs diversifiée
 fig = px.scatter_3d(
     df_embeddings,
-    x='x', y='y', z='z',
-    text='product_type',  # Ajouter des étiquettes aux points
-    color='product_type'  # Attribuer une couleur unique à chaque type de produit
+    x='PC1', y='PC2', z='PC3',
+    color='product_type',  # Attribuer une couleur unique à chaque type de produit
+    color_discrete_sequence=px.colors.qualitative.Bold  # Utilisation d'une palette de couleurs plus vives
 )
 
 # Personnalisation des marqueurs
 fig.update_traces(marker=dict(size=3))  # Réduction de la taille des points
 
-# Mise à l'échelle des axes pour qu'ils aient la même taille
+# Mise à l'échelle des axes pour qu'ils aient la même taille et ajout de noms aux axes
 fig.update_layout(
     scene=dict(
-        xaxis=dict(nticks=10, range=[-1,1]),
-        yaxis=dict(nticks=10, range=[-1,1]),
-        zaxis=dict(nticks=10, range=[-1,1]),
+        xaxis=dict(title='PC1', nticks=10, range=[-1,1]),
+        yaxis=dict(title='PC2', nticks=10, range=[-1,1]),
+        zaxis=dict(title='PC3', nticks=10, range=[-1,1]),
         aspectmode='cube'  # Cela forcera tous les axes à la même échelle
-    )
+    ),
+    legend_title=dict(text='Type de Produit'),
+    showlegend=True,
+    title='Visualisation 3D des Types de Produits par PCA'
 )
 
 # Afficher le graphique
