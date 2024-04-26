03b47b15ec13cb9ecc27cb0274d60ea6a714d59a
malayaan <117047767+malayaan@users.noreply.github.com>
2024-04-26 17:41:13 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,37 +1,35 @@
 import pandas as pd
 from itertools import combinations
+import numpy as np
 
 # ... [autre code] ...
 
-# Calculer le nombre total de combinaisons pour un groupe donné
-total_combinations_per_group = len(list(combinations(unique_product_types, 2)))
+# Initialisation de la matrice de co-occurrence avec numpy
+unique_product_types = df_clean['Product Type'].unique()
+co_occurrence_matrix_np = np.zeros((len(unique_product_types), len(unique_product_types)))
 
-# Calculer le nombre total de groupes
+# Création d'un mappage entre les types de produits et les indices de la matrice
+product_type_to_index = {product_type: index for index, product_type in enumerate(unique_product_types)}
+
+# Préparation pour l'indication de progression
 total_groups = df_clean['context'].nunique()
-
-# Le nombre total de combinaisons est le nombre de groupes multiplié par le nombre de combinaisons par groupe
-total_combinations = total_groups * total_combinations_per_group
-
-# Initialiser un compteur d'itérations
-count = 0
+group_count = 0
 
 # Remplir la matrice de co-occurrence
 for context, group in df_clean.groupby('context'):
-    product_types = group['Product Type'].tolist()
-    # Il faut recalculer les combinaisons pour chaque groupe car le nombre de types de produits peut varier
-    group_combinations = list(combinations(product_types, 2))
-    
-    for (product_type_1, product_type_2) in group_combinations:
-        co_occurrence_matrix.at[product_type_1, product_type_2] += 1
-        co_occurrence_matrix.at[product_type_2, product_type_1] += 1  # La matrice est symétrique
-        
-        # Mettre à jour le compteur d'itérations
-        count += 1
+    product_types_indices = [product_type_to_index[product_type] for product_type in group['Product Type']]
+    for (index1, index2) in combinations(product_types_indices, 2):
+        co_occurrence_matrix_np[index1, index2] += 1
+        co_occurrence_matrix_np[index2, index1] += 1  # La matrice est symétrique
 
-        # Calculer le pourcentage restant
-        percentage_done = (count / total_combinations) * 100
-        print(f"Progression : {percentage_done:.2f}%", end='\r')
+    # Mise à jour de la progression
+    group_count += 1
+    percentage_done = (group_count / total_groups) * 100
+    print(f"Progression : {percentage_done:.2f}%", end='\r')
+
+# Convertir la matrice numpy en DataFrame pour l'affichage
+co_occurrence_matrix = pd.DataFrame(co_occurrence_matrix_np, index=unique_product_types, columns=unique_product_types)
 
 # Afficher la matrice de co-occurrence
-print("\n")  # S'assurer de passer à la ligne après la fin de la progression
+print("\nTerminé.")
 print(co_occurrence_matrix)
