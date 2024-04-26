465d26a611aaa62854f16d76917d3e6371bb9a65
malayaan <117047767+malayaan@users.noreply.github.com>
2024-04-26 14:47:08 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,28 +1,30 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from itertools import combinations
 
-# Supposons que 'data_column' est la colonne sur laquelle vous effectuez value_counts()
-value_counts = df['data_column'].value_counts()
+# Supposons que df est votre DataFrame
+# Voici un exemple de données pour illustrer
+data = {
+    'Product_Type': ['A', 'B', 'A', 'C', 'B', 'D', 'C', 'A'],
+    'col1': [1, 1, 2, 2, 3, 3, 3, 1],
+    'col2': ['X', 'X', 'Y', 'Y', 'Z', 'Z', 'Z', 'X'],
+    'col3': ['alpha', 'alpha', 'beta', 'beta', 'gamma', 'gamma', 'gamma', 'alpha']
+}
+df = pd.DataFrame(data)
 
-# Regrouper les petits comptes en une seule catégorie 'Small Desk'
-small_counts_mask = value_counts < 1000
-small_counts = value_counts[small_counts_mask]
-value_counts = value_counts[~small_counts_mask]
+# Définir le contexte comme la combinaison de col1, col2 et col3
+df['context'] = df[['col1', 'col2', 'col3']].astype(str).agg('_'.join, axis=1)
 
-# Si 'Small Desk' existe déjà, ajoutez-y les petits comptes, sinon créez-la
-if 'Small Desk' in value_counts.index:
-    value_counts['Small Desk'] += small_counts.sum()
-else:
-    value_counts['Small Desk'] = small_counts.sum()
+# Initialiser la matrice de co-occurrence avec des zéros
+unique_product_types = df['Product_Type'].unique()
+co_occurrence_matrix = pd.DataFrame(0, index=unique_product_types, columns=unique_product_types)
 
-# Maintenant, value_counts a tous les grands comptes et une catégorie 'Small Desk' pour les petits
+# Remplir la matrice de co-occurrence
+for context, group in df.groupby('context'):
+    product_types = group['Product_Type'].tolist()
+    # Créer toutes les paires possibles dans le contexte
+    for (product_type_1, product_type_2) in combinations(product_types, 2):
+        co_occurrence_matrix.at[product_type_1, product_type_2] += 1
+        co_occurrence_matrix.at[product_type_2, product_type_1] += 1  # La matrice est symétrique
 
-# Créer un pie chart
-plt.pie(value_counts, labels=value_counts.index, autopct='%1.1f%%', startangle=140)
-plt.axis('equal')  # Assurer que le pie chart est un cercle
-
-# Ajouter un titre
-plt.title('Répartition des valeurs avec regroupement des petits comptes')
-
-# Afficher le diagramme
-plt.show()
+# Afficher la matrice de co-occurrence
+print(co_occurrence_matrix)
