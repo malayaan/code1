3d8ec363c3ffcee091faeb8f1aea1c1efd589f77
malayaan <117047767+malayaan@users.noreply.github.com>
2024-04-29 18:09:00 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,7 +1,11 @@
-### Data Description
+### Conclusion
 
-The data used in this analysis are sourced from three CSV files hosted in the 'DataFrame' directory of our Git repository. These files, named `Booking.csv`, `Referential.csv`, and `IT.csv`, encompass various incident reports relevant to their respective domains within Société Générale's operations. Each file was specifically designated to track and analyze the different aspects of data quality incidents—ranging from IT-related issues to booking and referential integrity problems.
+Our statistical analysis has pinpointed key opportunities for enhancing Société Générale's incident reporting process to reduce duplicates and improve efficiency. 
 
-The data were collected by Benjamin Pocart and Caroline Kao, who are responsible for the initial accumulation of incident reports. Subsequent preprocessing was conducted by Paul Decoux, detailed in the `DataCleaning` section of the Git repository. This preprocessing step was crucial for ensuring the data's usability for further analysis, particularly in preparing it for the duplicate detection process.
+First, implementing a feature that allows analysts to update existing reports, including attachments, analyses, or estimated impacts, could significantly decrease the need for duplicate submissions. This update functionality would preserve the continuity of incident tracking and maintain data integrity.
 
-These files contain extensive columns that record critical incident details such as dates, perimeters, root causes, templates used, and the analysts' explanations, along with the responses to these incidents. Such comprehensive data fields are instrumental in tracing and categorizing incidents, including the differentiation between potential true and faux duplicates. This categorization is key to our statistical analysis, aiming to enhance the overall efficiency of incident management by reducing redundancies and improving response time to genuine issues.
+Second, the pre-filled justification columns in the reporting templates need redesigning. These columns currently lack the detail necessary for clear statistical analysis. Enhancing these fields to capture more specific and granular information would improve data accuracy and utility.
+
+Lastly, a new pop-up feature would alert analysts when they select a template ID that matches one used for another incident on the same day and scope. This prompt would encourage them to verify whether they are about to create a duplicate, thereby minimizing redundancy.
+
+These improvements will streamline the incident management process at Société Générale, allowing the bank to focus resources on resolving genuine issues more efficiently.
