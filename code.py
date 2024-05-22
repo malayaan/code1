550f7613570592d606bfa91536ef5ca5e86487d5
malayaan <117047767+malayaan@users.noreply.github.com>
2024-05-22 11:28:18 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,17 +1,20 @@
-# Select the combinations of pricingdate and Gop in not_a_new_deal_situation
-not_a_new_deal_situation = df_concatenated[df_concatenated['SensisLastBusinessDayGop (Countervaluated)'].isnull()][['pricingdate', 'Gop']].drop_duplicates()
-
-# Create a set of tuples for fast lookup
-not_a_new_deal_set = set(not_a_new_deal_situation.apply(tuple, axis=1))
-
-# Define a function to apply
-def replace_na_if_not_in_set(row, lookup_set):
-    if pd.isna(row['SensisLastBusinessDayGop (Countervaluated)']) and (row['pricingdate'], row['Gop']) not in lookup_set:
-        return 0
-    return row['SensisLastBusinessDayGop (Countervaluated)']
-
-# Apply the function
-df_concatenated['SensisLastBusinessDayGop (Countervaluated)'] = df_concatenated.apply(lambda row: replace_na_if_not_in_set(row, not_a_new_deal_set), axis=1)
-
-# Display the modified DataFrame
-print(df_concatenated)
+Introduction
+Durée du stage : Deux mois chez Société Générale CIB en tant que Data Scientist.
+Projet principal : Automatisation de la classification des deals en "situation présentant un incident de qualité des données (DQ)" et "situation sans incident".
+Intérêt pour le sujet : Projet intéressant et pertinent, grande importance accordée par l'équipe.
+Reconnaissance : Appréciation notable pour les contributions apportées.
+Collaboration : Travail en binôme avec un autre stagiaire en data science, facilitant l'entraide et l'apprentissage mutuel.
+Points Positifs
+Sujet intéressant : Forte motivation grâce à l'intérêt de l'équipe pour le projet.
+Présence physique : Présence au bâtiment Basalte tous les mardis, facilitant le contact avec la partie métier et la résolution rapide des questions.
+Collaboration entre stagiaires : Entraide sur les sujets complexes, partage d'idées, et apprentissage mutuel, rendant l'expérience de stage enrichissante et collaborative.
+Points d'Amélioration
+Compréhension du métier d'analyste :
+Difficulté d'accès aux informations nécessaires pour bien comprendre les enjeux métiers.
+Questions laissées sans réponse sur Teams, ralentissant la progression.
+Importance de la compréhension du business :
+Clé pour le succès d'un stage en data science.
+Nécessité de connaître les besoins et contraintes métiers pour proposer des solutions pertinentes.
+Suggestions d'amélioration :
+Passer du temps directement au contact d'un analyste PnL pour mieux comprendre les problématiques métiers et favoriser des échanges plus constructifs.
+Mieux comprendre la partie data engineering de Caroline pour récupérer moi-même des données et être plus autonome dans le déblocage de certains points techniques.
