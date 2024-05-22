255581109fc1687ae4ce5afcbdecf9f7794ac8de
malayaan <117047767+malayaan@users.noreply.github.com>
2024-05-22 16:05:20 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
-z
+import re
+
+string = "joli je suis joli"
+pattern = r"(joli)"  # L'expression régulière avec le mot à conserver
+
+result = re.split(pattern, string)
+result = [segment for segment in result if segment]  # Enlever les segments vides si nécessaire
+
+print(result)
