23c9a2bdee0679c697e735166b2a2765b4122c8f
malayaan <117047767+malayaan@users.noreply.github.com>
2024-05-24 21:00:28 +0200

    modified:   mlp_training.py

--- mlp_training.py
+++ mlp_training.py
@@ -1,3 +1,8 @@
-Analyser les Commentaires : Identifier les deals problématiques à partir des notes et des anomalies.
-Standardiser et Labelliser : Uniformiser et marquer chaque situation.
-Entraîner le Modèle : Utiliser ces données pour créer un modèle de détection des problèmes de DQ
+1. Utilisation de Données Agrégées
+Approche : Analyser les données au niveau des groupes de portefeuilles (GOP) ou des portefeuilles (PTF).
+Solution : Créer des modèles pour chaque niveau et combiner les résultats.
+Exemple : Traiter toute la structure analytique comme un graph pour mieux comprendre les relations.
+2. Utilisation de Méthodes de Clustering
+Approche : Grouper les deals avec des comportements similaires.
+Solution : Utiliser des techniques de clustering pour identifier les groupes anormaux.
+Exemple : Appliquer K-means pour trouver des clusters de deals anormaux dans les GOP.
