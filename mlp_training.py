16c1e47713c951f4938c98f4bfd8c2f13880f26b
malayaan <117047767+malayaan@users.noreply.github.com>
2024-05-24 20:53:41 +0200

    modified:   mlp_training.py

--- mlp_training.py
+++ mlp_training.py
@@ -1,16 +1,3 @@
-Pourquoi Choisir le Niveau Deal pour la Précision
-Comparaison avec la Qualité des Plats
-Niveau de Précision Choisi : Le Deal
-
-Analogie du Plat : Pour étudier la qualité d'un plat, chaque problème de qualité est souvent lié à un ou plusieurs ingrédients spécifiques.
-Application aux Deals : De même, chaque problème de qualité des données (DQ) dans un PnL est souvent lié à un deal spécifique ou à plusieurs deals.
-Description de la Situation
-
-Analogie du Plat : Pour évaluer un plat, vous considérez à la fois la nature des ingrédients (caractéristiques des produits) et les caractéristiques globales du plat (caractéristiques agrégées).
-Application aux Deals : De même, pour décrire une situation de DQ, nous considérons les caractéristiques spécifiques du deal (nature du produit) et les caractéristiques agrégées des niveaux supérieurs (trading desk, portefeuille).
-Simplification de la Normalisation
-
-Analogie du Plat : Normaliser chaque situation est plus simple en se concentrant sur les ingrédients individuels plutôt que sur le plat entier, qui peut contenir de nombreux ingrédients.
-Application aux Deals : De même, normaliser chaque situation de DQ est plus simple au niveau des deals, car chaque portefeuille ou GOP peut contenir de nombreux deals.
-Conclusion
-En choisissant le niveau deal, nous assurons une précision et une homogénéité dans la description des situations de DQ. Cela nous permet de capturer à la fois les détails spécifiques (caractéristiques des deals) et les effets globaux (caractéristiques agrégées), tout en simplifiant le processus de normalisation.
+Analyser les Commentaires : Identifier les deals problématiques à partir des notes et des anomalies.
+Standardiser et Labelliser : Uniformiser et marquer chaque situation.
+Entraîner le Modèle : Utiliser ces données pour créer un modèle de détection des problèmes de DQ
