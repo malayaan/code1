907201fcffd42a1f773d4cce164707d49572ec33
malayaan <117047767+malayaan@users.noreply.github.com>
2024-06-28 14:52:03 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,12 +1,47 @@
-# Groupement par 'gop' et 'portfolio' et comptage des incidents
-gop_portfolio_counts = df.groupby(['gop', 'portfolio']).size().unstack(fill_value=0)
+# Comptage des incidents par GOP
+gop_incidents = df['gop'].value_counts()
 
-# Affichage sous forme de heatmap pour une meilleure visualisation
-import seaborn as sns
+# Visualisation
+gop_incidents.plot(kind='bar', color='skyblue')
+plt.title('Nombre d’Incidents par GOP')
+plt.xlabel('GOP')
+plt.ylabel('Nombre d’Incidents')
+plt.xticks(rotation=45)
+plt.show()
 
-plt.figure(figsize=(12, 8))
-sns.heatmap(gop_portfolio_counts, annot=True, fmt="d", cmap='viridis')
-plt.title('Distribution des Incidents par GOP et Portfolio')
+
+# Comptage des incidents par Portfolio
+portfolio_incidents = df['portfolio'].value_counts()
+
+# Visualisation
+portfolio_incidents.plot(kind='bar', color='green')
+plt.title('Nombre d’Incidents par Portfolio')
 plt.xlabel('Portfolio')
-plt.ylabel('GOP')
+plt.ylabel('Nombre d’Incidents')
+plt.xticks(rotation=45)
+plt.show()
+
+# Préparation des données
+df['month'] = df['startTime'].dt.to_period('M')
+
+# Aggrégation des données
+incidents_time_gop = df.groupby(['month', 'gop']).size().unstack().fillna(0)
+
+# Visualisation
+incidents_time_gop.plot(kind='line', marker='o')
+plt.title('Tendance des Incidents par Mois et par GOP')
+plt.xlabel('Mois')
+plt.ylabel('Nombre d’Incidents')
+plt.legend(title='GOP')
+plt.show()
+
+# Aggrégation des données
+type_gop_distribution = pd.crosstab(df['gop'], df['type'])
+
+# Visualisation avec un stacked bar chart
+type_gop_distribution.plot(kind='bar', stacked=True)
+plt.title('Répartition des Types d’Incidents par GOP')
+plt.xlabel('GOP')
+plt.ylabel('Nombre d’Incidents')
+plt.xticks(rotation=0)
 plt.show()
