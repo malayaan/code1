50a46dc37b5fc0936516f3b6b8a03e960a80c28d
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-03 16:12:42 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,5 @@
+pip uninstall torch torchvision torchaudio
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu123
 import torch
-
-# Vérifie si CUDA est disponible
-cuda_available = torch.cuda.is_available()
-print("CUDA disponible:", cuda_available)
-
-# Affiche le nombre de GPU disponibles et leurs noms si CUDA est disponible
-if cuda_available:
-    num_gpus = torch.cuda.device_count()
-    print("Nombre de GPU(s) disponible(s):", num_gpus)
-    for i in range(num_gpus):
-        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
-else:
-    print("Aucun GPU compatible CUDA n'est disponible.")
+print(torch.__version__)
+print(torch.cuda.is_available())
