bd7901ed29698ae07da230f89bcd802f5633d7c9
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-03 16:45:10 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,2 @@
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118
+pip uninstall torch torchvision torchaudio
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
