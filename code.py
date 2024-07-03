ff3f6985bd47d5d8ccd786290348a8592ee2d2c6
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-03 18:08:56 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,2 +1,1 @@
-pip uninstall torch torchvision torchaudio
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+Get-WmiObject win32_VideoController | Format-List Name, AdapterCompatibility
