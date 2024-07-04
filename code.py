8b4641ca156a0ee2fe93cdf94554935db0ee3b93
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-04 15:42:24 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,1 +1,21 @@
-Get-WmiObject win32_VideoController | Format-List Name, AdapterCompatibility
+Situation Actuelle et Défis en Gestion des Risques Financiers
+
+Contexte Opérationnel :
+Chaque jour, les traders de notre entreprise prennent des positions financières qui sont évaluées pour déterminer leur valeur, connue sous le nom de Profit and Loss (PnL). Pour chaque position, des "effets du PnL" sont également calculés pour expliquer les variations de valorisation. Toutefois, la précision et la fiabilité de ces données sont cruciales et doivent être vérifiées par un analyste pour s'assurer de leur exactitude.
+
+Problèmes de Data Quality :
+Les données utilisées pour calculer le PnL et les effets associés peuvent souffrir de plusieurs types d'erreurs, incluant :
+
+Erreurs de saisie : Des erreurs humaines lors de l'entrée des données.
+Erreurs d'alimentation des données de marché : Des problèmes dans les flux de données externes utilisés pour les évaluations.
+Erreurs de traitement des données : Défaillances dans les logiciels ou les algorithmes qui traitent les données.
+Certification des Données :
+Cette vérification de la qualité des données s'applique non seulement au PnL mais aussi à environ 200 métriques de risques différents qui évaluent l'exposition de la position à divers aspects du risque financier. Ces métriques varient selon les spécificités de chaque situation.
+
+Processus de Détection des Problèmes de Data Quality :
+La détection des incidents de qualité des données est extrêmement exigeante en termes de temps :
+
+Analyse Hiérarchique : L'analyse commence au niveau des groupes de portefeuilles pour identifier toute valeur suspecte. Si des anomalies sont détectées, l'analyse descend au niveau des portefeuilles individuels, et ensuite aux deals spécifiques pour localiser les incohérences.
+Millions de Deals Quotidiens : Avec des millions de deals évalués chaque jour par trading desk, le processus doit être efficace et systématique pour être gérable.
+Objectif du Processus :
+L'objectif est de détecter les situations présentant un risque élevé d'incident de qualité des données basé sur des incidents historiquement signalés. Ces situations sont ensuite priorisées et présentées aux analystes dans les applications de certification, leur permettant ainsi de se concentrer sur les cas les plus critiques et de gagner un temps précieux dans leur travail quotidien.
