e032a5987bcdb7d61708c00266de931bd8bcb365
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-08 16:02:19 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 # Supposons que df_incident est votre DataFrame
 type_gop_distribution = pd.crosstab(df_incident['gop'], df_incident['type'])
 
-# Calculer la somme des incidents pour chaque type et trier les valeurs
-top_types = type_gop_distribution.sum(axis=0).sort_values(ascending=False).head(10)
+# Calculer la somme des incidents pour chaque gop (somme sur les colonnes pour chaque gop, axis=1)
+top_gops = type_gop_distribution.sum(axis=1).sort_values(ascending=False).head(10)
 
-# Filtrer le DataFrame original pour ne garder que les top types
-filtered_gop_distribution = type_gop_distribution[top_types.index]
+# Filtrer le DataFrame original pour ne garder que les top gops
+filtered_gop_distribution = type_gop_distribution.loc[top_gops.index]
 
 # Créer le graphique
 filtered_gop_distribution.plot(kind='bar', stacked=True, figsize=(10,5))
-plt.title('Composition of incident type by gop (Top 10 Total Incidents)')
+plt.title('Composition of incident by top 10 GOPs')
 plt.xlabel('GOP')
 plt.ylabel('Incidents count')
 plt.xticks(rotation=0)
