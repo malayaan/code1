9fab0bcd2151dd05fd7e7209795bfd8823e97bb0
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-11 15:46:35 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,40 +1,22 @@
 import pandas as pd
 
-# Simulation des données d'entraînement et de test
-data_train = {
-    'underlyingtype': ['Equity', 'Bond', 'Equity', 'Bond', 'Equity', 'Bond'],
-    'producttype': ['Option', 'Future', 'Option', 'Future', 'Option', 'Future'],
-    'type': ['Incident', 'No Incident', 'No Incident', 'Incident', 'Incident', 'No Incident']
-}
-data_test = {
-    'underlyingtype': ['Equity', 'Bond', 'Equity', 'Bond'],
-    'producttype': ['Option', 'Future', 'Option', 'Future'],
-    'type': ['Incident', 'Incident', 'No Incident', 'No Incident']
-}
+# Supposons que vous avez déjà vos dataframes X_train, X_test, y_train, y_test
+# et que 'type' est votre variable cible, jointe avec X_train pour faciliter le groupby
+X_train['type'] = y_train
 
-X_train = pd.DataFrame(data_train)
-y_train = X_train.pop('type')  # Si 'type' est la cible, nous devons l'encoder numériquement pour le mean encoding
-X_test = pd.DataFrame(data_test)
-y_test = X_test.pop('type')
+# Calculer le mean encoding pour 'underlyingtype'
+mean_underlyingtype = X_train.groupby('underlyingtype')['type'].mean()
+X_train['underlyingtype_encoded'] = X_train['underlyingtype'].map(mean_underlyingtype)
+X_test['underlyingtype_encoded'] = X_test['underlyingtype'].map(mean_underlyingtype).fillna(X_train['type'].mean())
 
-# Mapping des types d'incidents en valeurs numériques pour y_train
-type_mapping = {'Incident': 1, 'No Incident': 0}
-y_train = y_train.map(type_mapping)
-y_test = y_test.map(type_mapping)
+# Calculer le mean encoding pour 'product type'
+mean_producttype = X_train.groupby('product type')['type'].mean()
+X_train['producttype_encoded'] = X_train['product type'].map(mean_producttype)
+X_test['producttype_encoded'] = X_test['product type'].map(mean_producttype).fillna(X_train['type'].mean())
 
-# Calcul du mean encoding pour 'underlyingtype' et 'producttype' en utilisant y_train
-mean_encode_underlying = X_train.groupby('underlyingtype')['type'].mean()
-mean_encode_product = X_train.groupby('producttype')['type'].mean()
+# Supprimer la colonne 'type' ajoutée temporairement à X_train si nécessaire
+X_train.drop('type', axis=1, inplace=True)
 
-# Appliquer le mean encoding sur X_train et X_test
-X_train['underlyingtype_encoded'] = X_train['underlyingtype'].map(mean_encode_underlying)
-X_train['producttype_encoded'] = X_train['producttype'].map(mean_encode_product)
-
-X_test['underlyingtype_encoded'] = X_test['underlyingtype'].map(mean_encode_underlying)
-X_test['producttype_encoded'] = X_test['producttype'].map(mean_encode_product)
-
-# Affichage des DataFrames après l'encodage
-print("X_train with mean encoding:")
-print(X_train)
-print("\nX_test with mean encoding:")
-print(X_test)
+# Vérification : affichage des premières lignes pour contrôler
+print(X_train.head())
+print(X_test.head())
