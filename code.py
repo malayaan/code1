5ed5de29f41c07b74e6340c95d4771a05f898ffb
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-11 17:43:09 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,7 +1,27 @@
-# Fusionner toutes les chaînes de df2 en une expression régulière
-import re
-pattern = '|'.join(df2['ints'].tolist())  # Crée une chaîne comme '123|789|654'
+import pandas as pd
+from tqdm import tqdm
 
-# Filtrer df1 pour les lignes contenant un des motifs
-df1['match'] = df1['strings'].apply(lambda x: bool(re.search(pattern, x)))
+# Création de DataFrame exemple df1
+data1 = {
+    'strings': ['hello 123', 'world 456', 'test 789', 'example 101']
+}
+df1 = pd.DataFrame(data1)
+
+# Création de DataFrame exemple df2
+data2 = {
+    'ints': [123, 789, 654]
+}
+df2 = pd.DataFrame(data2)
+
+# Convertir les entiers en chaînes
+df2['ints_str'] = df2['ints'].astype(str)
+pattern = '|'.join(df2['ints_str'].tolist())  # '123|789|654'
+
+# Utiliser str.contains pour filtrer df1
+tqdm.pandas(desc="Matching strings")
+df1['match'] = df1['strings'].progress_apply(lambda x: bool(re.search(pattern, x)))
+
+# Filtrer pour obtenir les résultats
 filtered_df1 = df1[df1['match']]
+
+print(filtered_df1)
