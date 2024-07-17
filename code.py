907c9bf9d3df2a942e1e8c2ce008b50b251ad241
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-17 11:22:21 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,24 +1,4 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-import numpy as np
-
-# Assuming filtered_df is already defined and contains the data to be plotted
-
-plt.figure(figsize=(10, 8))
-well_knowned = x_train_string['Underlying Name'].values
-filtered_df = embedding_df[embedding_df['Underlying Name'].isin(well_knowned)][['Underlying_name_embedding_1', 'Underlying_name_embedding_2']]
-
-plt.figure(figsize=(8, 6))
-x = filtered_df['Underlying_name_embedding_1']
-y = filtered_df['Underlying_name_embedding_2']
-
-# Create a 2D histogram (density plot)
-nbins = 100
-kde = sns.kdeplot(x=x, y=y, cmap='Reds', fill=True)
-plt.colorbar(kde.collections[-1], ax=kde, orientation='vertical').set_label('Density')
-
-plt.xlabel('Underlying_name_embedding_1')
-plt.ylabel('Underlying_name_embedding_2')
-plt.title('Density Plot with Colorbar')
-plt.show()
+X_train = X_train.reset_index(drop=True)
+X_test = X_test.reset_index(drop=True)
+y_train = y_train.reset_index(drop=True)
+y_test = y_test.reset_index(drop=True)
