0d9b600c81bcf3fba47c6fa1f2a5c03aba67367f
malayaan <117047767+malayaan@users.noreply.github.com>
2024-07-17 10:51:07 +0200

    modified:   code.py

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 # Assuming filtered_df is already defined and contains the data to be plotted
 
@@ -9,15 +10,15 @@ well_knowned = x_train_string['Underlying Name'].values
 filtered_df = embedding_df[embedding_df['Underlying Name'].isin(well_knowned)][['Underlying_name_embedding_1', 'Underlying_name_embedding_2']]
 
 plt.figure(figsize=(8, 6))
-ax = sns.kdeplot(
-    x=filtered_df['Underlying_name_embedding_1'], 
-    y=filtered_df['Underlying_name_embedding_2'], 
-    cmap='Reds', 
-    fill=True
-)
+x = filtered_df['Underlying_name_embedding_1']
+y = filtered_df['Underlying_name_embedding_2']
 
-# Add a colorbar to indicate density
-cbar = plt.colorbar(ax.collections[0], ax=ax, orientation='vertical')
-cbar.set_label('Density')
+# Create a 2D histogram (density plot)
+nbins = 100
+kde = sns.kdeplot(x=x, y=y, cmap='Reds', fill=True)
+plt.colorbar(kde.collections[-1], ax=kde, orientation='vertical').set_label('Density')
 
+plt.xlabel('Underlying_name_embedding_1')
+plt.ylabel('Underlying_name_embedding_2')
+plt.title('Density Plot with Colorbar')
 plt.show()
