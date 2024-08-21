68c36586c27dede1dba78d372fc55aa0e209237d
malayaan <117047767+malayaan@users.noreply.github.com>
2024-08-21 17:01:12 +0200

    modified:   code_1.py

--- code_1.py
+++ code_1.py
@@ -1,20 +1,38 @@
-Dans le cadre de ce stage, la revue de la littérature sur les réseaux neuronaux pour graphes (Graph Convolutional Networks, GCNs) a été une composante essentielle pour établir la base théorique et méthodologique de notre projet. Plusieurs articles clés ont guidé notre exploration et compréhension des GCNs :
+import numpy as np
+import matplotlib.pyplot as plt
 
-1. **Kipf, T.N. & Welling, M. (2017).** "Semi-Supervised Classification with Graph Convolutional Networks." Cet article est souvent considéré comme fondateur pour les GCNs modernes, introduisant une méthode qui utilise une forme de convolution sur les graphes pour traiter efficacement les données semi-supervisées.
+# Example data
+y_true = np.array([0, 1, 0, 1, 0, 1, 0, 1, 1])
+y_pred = np.array([0, 1, 1, 1, 0, 0, 0, 1, 1])
 
-2. **Hamilton, W., Ying, R., & Leskovec, J. (2017).** "Inductive Representation Learning on Large Graphs." Publié à NIPS, cet article a exploré les méthodes inductives de l'apprentissage sur les graphes, ce qui a permis d'étendre les applications des GCNs à des graphes de grande échelle non vus lors de l'entraînement.
-
-3. **Veličković, P., Cucurull, G., Casanova, A., Romero, A., Liò, P., & Bengio, Y. (2018).** "Graph Attention Networks." Introduisant les mécanismes d'attention dans les GCNs, cet article a permis de moduler l'importance des nœuds voisins sans nécessiter de transformations radicales de l'architecture, améliorant ainsi la flexibilité et la performance des modèles.
-
-Ces articles ont non seulement fourni un aperçu des capacités des GCNs mais ont aussi souligné les défis associés, tels que la gestion de la complexité computationnelle et la nécessité d'approches capables de gérer des dépendances à long terme dans les données de graphes. En se basant sur ces fondements, notre projet a exploré comment ces techniques pouvaient être adaptées pour détecter les anomalies dans les données financières structurées sous forme de graphes, reflétant ainsi l'engagement continu dans une recherche appliquée rigoureuse et orientée vers des solutions concrètes pour la gestion des risques financiers.
-
-
+# Calculate true positives, false positives, true negatives, and false negatives
+TP = np.sum((y_true == 1) & (y_pred == 1))  # True Positive
+TN = np.sum((y_true == 0) & (y_pred == 0))  # True Negative
+FP = np.sum((y_true == 0) & (y_pred == 1))  # False Positive
+FN = np.sum((y_true == 1) & (y_pred == 0))  # False Negative
 
 
-Au début de mon stage, conscient de l'importance de construire une solide base de connaissances, je me suis plongé dans l'étude des réseaux de neurones convolutifs pour graphes (Graph Convolutional Networks, GCNs). Mon objectif était de comprendre en profondeur les principes sous-jacents et les applications potentielles de ces technologies avant de les mettre en œuvre.
+# Pie chart for real zeros of y_true
+labels = 'True Negatives', 'False Positives'
+sizes = [TN, FP]
+colors = ['lightblue', 'lightcoral']
+explode = (0.1, 0)  # explode the first slice
 
-1. **Recherche Bibliographique** : J'ai consulté des articles de référence pour me former sur les GCNs, parmi lesquels figurent des travaux de Kipf et Welling (2017) sur la classification semi-supervisée avec les GCNs, ainsi que les recherches de Hamilton et al. (2017) sur l'apprentissage de représentation inductive sur de grands graphes. Ces lectures m'ont permis de saisir les innovations et les défis techniques associés à l'application des GCNs dans des contextes réels et complexes.
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)  # 1 row, 2 columns, first subplot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title('Distribution for True Zeros in y_true')
 
-2. **Expérimentation Pratique** : Pour mettre en pratique ces connaissances théoriques, j'ai entrepris de faire tourner un algorithme de GCN sur des données fictives. Cet exercice pratique était crucial pour intégrer concrètement les concepts appris et tester la viabilité des méthodes sur des exemples contrôlés, ce qui m'a aidé à mieux comprendre les interactions et les performances possibles des GCNs dans des scénarios simulés.
+# Pie chart for real ones of y_true
+labels = 'True Positives', 'False Negatives'
+sizes = [TP, FN]
+colors = ['gold', 'lightgreen']
+explode = (0.1, 0)  # explode the first slice
 
-Cette phase initiale d'apprentissage et d'expérimentation m'a non seulement équipé avec les outils nécessaires pour aborder les défis du projet mais a également établi une fondation solide pour les phases subséquentes de développement et d'application des modèles au contexte spécifique de notre entreprise. Cette approche méthodique garantissait que toute intégration de GCN dans nos systèmes serait basée sur une compréhension approfondie et une évaluation rigoureuse de la technologie.
+plt.subplot(1, 2, 2)  # 1 row, 2 columns, second subplot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.title('Distribution for True Ones in y_true')
+
+plt.show()
