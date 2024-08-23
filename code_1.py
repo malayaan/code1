42d1668579814de5bb90bb7f0701921d50e12afa
malayaan <117047767+malayaan@users.noreply.github.com>
2024-08-23 14:36:20 +0200

    modified:   code_1.py

--- code_1.py
+++ code_1.py
@@ -1,31 +1,15 @@
-import matplotlib.pyplot as plt
+Dans le cadre de mon analyse statistique des rapports d'incidents au sein de la banque, il est crucial de comprendre la structure organisationnelle des unités opérationnelles concernées :
 
-def calculate_confusion_matrix(y_true, y_pred):
-    TP = np.sum((y_true == 1) & (y_pred == 1))
-    TN = np.sum((y_true == 0) & (y_pred == 0))
-    FP = np.sum((y_true == 0) & (y_pred == 1))
-    FN = np.sum((y_true == 1) & (y_pred == 0))
-    return TP, TN, FP, FN
+1. **Trading Desk (GRPP200)** : Un trading desk est une unité spécifique au sein de la banque où les traders réalisent des opérations financières sur différents types de produits. C'est le niveau le plus élevé de spécificité dans la gestion des opérations de trading.
 
-# Boucle pour chaque groupe pour calculer les métriques et générer des graphiques
-for group in results.keys():
-    TP, TN, FP, FN = calculate_confusion_matrix(y_test[X_set_test['ProductTypeGroup'] == group].values.ravel(), results[group]['test_predictions'])
+2. **Profit Center (PC)** : Ce terme désigne un centre de profit qui est responsable de la gestion des revenus et des coûts associés à une partie spécifique des opérations de trading. Un PC peut comprendre plusieurs groupes de portefeuilles.
 
-    # Calculs pour les pie charts
-    # Pour les non-anomalies initialement non suspectées
-    labels = ['True Non-Anomalies', 'False Anomalies']
-    sizes = [TN, FP]
-    colors = ['green', 'red']
-    plt.figure(figsize=(7, 7))
-    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-    plt.title(f'Non-Anomalies Initially Flagged as Non-Suspect for {group}')
-    plt.show()
+3. **Groupe de Portefeuilles (GOP)** : Un GOP regroupe plusieurs portefeuilles qui sont gérés ensemble en raison de leurs stratégies ou de leurs objectifs d'investissement similaires. C'est souvent à ce niveau que les incidents sont déclarés.
 
-    # Pour les anomalies initialement suspectées
-    labels = ['True Anomalies', 'False Non-Anomalies']
-    sizes = [TP, FN]
-    colors = ['blue', 'orange']
-    plt.figure(figsize=(7, 7))
-    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-    plt.title(f'Anomalies Initially Flagged as Suspect for {group}')
-    plt.show()
+4. **Portefeuille** : Il s'agit d'un ensemble de deals ou transactions financières gérées comme une unité unique par un gestionnaire de portefeuille.
+
+Au cours de mon stage, l'analyse a révélé que la plupart des incidents de data quality (DQ) sont déclarés au niveau des GOP, tandis que les déclarations au niveau des portefeuilles sont moins fréquentes. En discutant avec les analystes, j'ai progressivement compris que la classification des incidents en DQ repose souvent sur des critères appliqués au niveau des deals, même si la complexité générale de ces classifications rend le processus difficile à saisir pleinement.
+
+Étant donné les contraintes de mon stage, principalement positionné dans l'équipe IT et non directement avec les analystes, il n'était pas possible d'acquérir une compréhension approfondie du métier d'analyste sans une organisation préalable. Ce manque de compréhension détaillée des processus métier a représenté un obstacle tout au long de mon stage.
+
+Parmi les différents trading desks analysés, EQDEUR1D s'est distingué par le volume le plus élevé d'incidents, particulièrement au sein du GOP XF. Les produits gérés dans ce périmètre sont relativement simples, principalement des equities, où le travail des analystes est censé être plus direct. Heureusement, j'ai pu établir un contact direct avec un analyste de ce périmètre, qui a fourni des éclaircissements utiles tout au long de mon stage, facilitant ainsi ma compréhension des enjeux et des processus impliqués.
