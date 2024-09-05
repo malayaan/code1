b15f64efaf585b97e80c4ccda4b31629e1446f74
malayaan <117047767+malayaan@users.noreply.github.com>
2024-09-05 12:00:14 +0200

    modified:   code_1.py

--- code_1.py
+++ code_1.py
@@ -1,5 +1,13 @@
-Warning: Handling Booking Incidents from SGWorkflow
+Required Modules
+You can install these using pip. Here is the list with installation commands:
 
-Please be aware that booking incidents retrieved from SGWorkflow have an asof date that is delayed by one day (i.e., the data reflects the state as of J+1). This is a known issue that should be addressed in a future update.
-
-In the meantime, exercise caution when retrieving new data from this source, as this date misalignment may impact time-sensitive analyses or reporting
+pip install pandas
+pip install scikit-learn
+pip install numpy
+pip install matplotlib
+pip install seaborn
+pip install optuna
+pip install skopt
+pip install tkinter
+pip install scikit-optimize
+pip install calendar
